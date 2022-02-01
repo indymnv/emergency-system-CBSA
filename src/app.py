dff26995076c7256e28ff620f3d4dad0f6d76fba
indymnv <indy.mnv@gmail.com>
2022-01-31 21:09:03 -0300

    adding filter in sidebar and connecting with dashboard

--- src/app.py
+++ src/app.py
@@ -5,6 +5,7 @@ import plotly.figure_factory as ff
 import plotly.express as px
 from pathlib import Path
 import os
+import datetime
 
 st.title("Sistema de control de emergencias CBSA")
 
@@ -28,7 +29,6 @@ def load_data():
     data['lon']=data['lon'].astype(float)
     return data
 
-
 #Cantidad de emergencias
 # Create a text element and let the reader know the data is loading.
 data_load_state = st.text('cargando data..')
@@ -36,19 +36,31 @@ data_load_state = st.text('cargando data..')
 data = load_data()
 data_load_state.text('Data cargada!')
 
+
+start_date = st.sidebar.date_input(
+        "Fecha de inicio",
+        datetime.date(2021,1,1)
+        )
+end_date = st.sidebar.date_input(
+        "Fecha de término",
+        datetime.date(2023,1,1)
+        )
+
+df_filtered = data[(data["Fecha"] > pd.to_datetime(start_date)) & (data["Fecha"] < pd.to_datetime(end_date))] 
 # Notify the reader that the data was successfully loaded.
 
-col1, col2, col3  = st.columns(3)
-col1.metric("Total Emergencias", value = data.shape[0])
-col2.metric("Tiempo Respuesta Promedio (minutos)", value = round(data["Tiempo respuesta"].mean(),2))
-col3.metric("Tiempo control Promedio (minutos)", value = round(data["Tiempo en controlar emergencia"].mean(),2))
 
-if st.checkbox('Mostrar data'):
+col1, col2, col3  = st.columns(3)
+col1.metric("Total Emergencias", value = df_filtered.shape[0])
+col2.metric("Tiempo Respuesta Promedio (minutos)", value = round(df_filtered["Tiempo respuesta"].median(),2))
+col3.metric("Tiempo control Promedio (minutos)", value = round(df_filtered["Tiempo en controlar emergencia"].mean(),2))
+
+if st.checkbox('Mostrar 10 últimas emergencias registradas'):
     st.subheader('Raw data')
-    st.write(data)
+    st.write(df_filtered.tail(10))
 
 st.subheader('Cantidad de emergencias por mes')
-dft1= pd.DataFrame(data['Fecha'].dt.to_period('M').value_counts().sort_index())
+dft1= pd.DataFrame(df_filtered['Fecha'].dt.to_period('M').value_counts().sort_index())
 dft1.columns = ['Total']
 dft1.index = dft1.index.astype(str, copy=False)
 st.line_chart(dft1)
@@ -57,6 +69,6 @@ fig = px.line(dft1)
 st.write()
 
 st.text('Mapa de emergencias')
-st.map(data)
+st.map(df_filtered)
 
 

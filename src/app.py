b0781164030ca36f1ac0dad1de5d47a1554e7c01
indymnv <indy.mnv@gmail.com>
2022-09-10 08:39:39 +0900

    updating app

--- src/app.py
+++ src/app.py
@@ -1,3 +1,4 @@
+from email.utils import collapse_rfc2231_value
 import pandas as pd
 import numpy as np
 import streamlit as st
@@ -8,26 +9,32 @@ import os
 import datetime
 import preprocessor
 
-st.title("Sistema de control de emergencias CBSA")
+st.title("Registro de Emergencias CBSA")
 
 #Config
 DATE_COLUMN = 'Fecha'
-DATA_URL = os.path.join(os.path.dirname(__file__), '..', 'data', 'emergencias-2021.csv')
+DATA_URL = os.path.join(os.path.dirname(__file__), '..', 'data', 'cleaned_data.csv')
 #Cantidad de emergencias
 # Create a text element and let the reader know the data is loading.
 #@st.cache
 
 # Load 10,000 rows of data into the dataframe.
-data = preprocessor.load_data(DATA_URL, DATE_COLUMN)
+data = pd.read_csv(DATA_URL)
+data.Fecha = pd.to_datetime(data.Fecha)
+data.Hora_del_llamado.fillna(data["Hora_de_primera_respuesta"],inplace=True) #first fill
+data.Hora_del_llamado.fillna(data["Hora_de_término_de_la_emergencia"],inplace=True) # second fill 
+data["int_hour"] = data.Hora_del_llamado.str[:2].astype(int)
+data = data.sort_values('Fecha')
 
 #add calendar dates in side bar
+st.sidebar.write("Seleccione los filtros deseados")
 start_date = st.sidebar.date_input(
         "Fecha de inicio",
-        datetime.date(2021,1,1)
+        datetime.date(2020,1,1)
         )
 end_date = st.sidebar.date_input(
         "Fecha de término",
-        datetime.date(2023,1,1)
+        datetime.date(2022,6,1)
         )
 
 time_step = st.sidebar.slider(label = "Periodo del tiempo en un día",
@@ -35,15 +42,24 @@ time_step = st.sidebar.slider(label = "Periodo del tiempo en un día",
         max_value = 24,
         value = (0,24))
 
+options = st.sidebar.multiselect(
+     'Seleccione el tipo de emergencias que quiere visualizar',
+     [10.0, 10.1, 10.2, 10.3, 10.4, 10.5, 10.6, 10.7, 10.8, 10.9, 10.12],
+     [10.0, 10.1, 10.2, 10.3, 10.4, 10.5, 10.6, 10.7, 10.8, 10.9, 10.12])
 
-df_filtered = data[(data["Fecha"] > pd.to_datetime(start_date)) & (data["Fecha"] < pd.to_datetime(end_date)) & (data['int_hour']>= int(time_step[0])) & (data['int_hour']<= int(time_step[1])) ] 
+
+
+
+df_filtered = data[(data["Fecha"] > pd.to_datetime(start_date)) & (data["Fecha"] < pd.to_datetime(end_date)) 
+                        & (data['int_hour']>= int(time_step[0])) & (data['int_hour']<= int(time_step[1])) &
+                        (data["Emergencias_cod_corto"].isin(options)) ] 
 # Notify the reader that the data was successfully loaded.
 
 
 col1, col2, col3  = st.columns(3)
 col1.metric("Total Emergencias", value = df_filtered.shape[0])
-col2.metric("Tiempo Respuesta Promedio (minutos)", value = round(df_filtered["Tiempo respuesta"].median(),2))
-col3.metric("Tiempo control Promedio (minutos)", value = round(df_filtered["Tiempo en controlar emergencia"].mean(),2))
+col2.metric("Tiempo Respuesta Promedio (min)", value = round(df_filtered["Tiempo_respuesta"].median(),2))
+col3.metric("Tiempo control Promedio (min)", value = round(df_filtered["Tiempo_en_controlar_emergencia"].mean(),2))
 
 if st.checkbox('Mostrar 10 últimas emergencias registradas'):
     st.subheader('Raw data')
@@ -55,22 +71,34 @@ st.subheader('Cantidad de emergencias por mes')
 df2 = preprocessor.general_line_chart(df_filtered)
 st.line_chart(df2)
 
+st.subheader('Cantidad de Emergencias por Compañía')
 chart_data = df_filtered[['1cia', '2cia', '3cia', '4cia']].sum() 
 st.bar_chart(chart_data, use_container_width=True)
 
-colA, colB = st.columns(2)
+
 #Insert line chart with total by emergencies
+st.subheader('Cantidad de emergencias mensual por clasificación')
 df3 = preprocessor.line_chart_by_emergencies(df_filtered)
-colA.line_chart(df3)
+st.line_chart(df3)
 
 #stacked bar chart
+st.subheader('Share de emergencias por clasificación')
 df_stacked = round(df3[df3.columns].apply(lambda x: x/x.sum(), axis=1)*100,2)
-colB.bar_chart(df_stacked)
+st.bar_chart(df_stacked)
+
+col1, col2 = st.columns(2)
+col1.write("Mediana tiempo respuesta (min)")
+df_time_respuesta = preprocessor.line_chart_for_time(df_filtered, "Tiempo_respuesta")
+col1.line_chart(df_time_respuesta)
+
+col2.write("Mediana Tiempo de control (min)")
+df_time_control = preprocessor.line_chart_for_time(df_filtered, "Tiempo_en_controlar_emergencia")
+col2.line_chart(df_time_control)
 
 fig = px.line(df2)
 st.write()
 
-st.subheader('Mapa de emergencias')
-st.map(df_filtered)
+#st.subheader('Mapa de emergencias')
+#st.map(df_filtered)
 
 
--- src/preprocessor.py
+++ src/preprocessor.py
@@ -2,19 +2,20 @@ import pandas as pd
 import numpy as np
 
 def load_data(DATA_URL, DATE_COLUMN):
-    data = pd.read_csv(DATA_URL, encoding="latin-1", sep=';')
-    data['Fecha'] = pd.to_datetime(data["Fecha"])
-    data['mes'] = data[DATE_COLUMN].dt.month_name()
-    data['Periodo']=  data['Fecha'].dt.to_period('M')
+    data = pd.read_csv(DATA_URL, sep=';')
+    #data['Fecha'] = pd.to_datetime(data["Fecha"])
+    #data['mes'] = data[DATE_COLUMN].dt.month_name()
+    #data['Periodo']=  data['Fecha'].dt.to_period('M')
     data = data.sort_values('Fecha')
     data['int_hour'] = data['Hora del llamado'].str[:2].astype(int) 
     data.rename(columns = {'LATITUDE': 'lat', 'LONGITUDE':'lon'}, inplace = True)
     
-    data['lat']=data['lat'].str.replace(",",".", regex = True)
-    data['lon']=data['lon'].str.replace(",", ".", regex = True)
-    data['lat']=data['lat'].astype(float)
-    data['lon']=data['lon'].astype(float)
+    #data['lat']=data['lat'].str.replace(",",".", regex = True)
+    #data['lon']=data['lon'].str.replace(",", ".", regex = True)
+    #data['lat']=data['lat'].astype(float)
+    #data['lon']=data['lon'].astype(float)
     return data
+
 
 def general_line_chart(df):
     
@@ -24,33 +25,14 @@ def general_line_chart(df):
     return dft1
 
 def line_chart_by_emergencies(df):
-    df['Emergencias_cod_corto'] = df['Tipo de emergencias']
-    mapper = {'10.6.1': '10.6',
-          '10.2.1': '10.2', 
-          '10.5': '10.5',
-          '10.0': '10.0',
-          '10.4.1': '10.4',
-          '10.4.3': '10.4',
-          '10.7': '10.7',
-          '10.4.2': '10.4',
-          '10.1': '10.1',
-          '10.6.2': '10.6',
-          '10.3.1': '10.3',
-          '10.8': '10.8',
-          '10.12X10.2.2': '10.2',
-          '10.2.2': '10.2',
-          '10.12X10.0': '10.0',
-          '10.5.1': '10.5',
-          '10.12X10.2.1': '10.2',
-          '10.9': '10.9',
-          '10.10': '10.10'}
+    #df['Emergencias_cod_corto'] = df['Emergencias_cod_corto']
 
-    df['Emergencias_cod_corto'] = df['Emergencias_cod_corto'].map(mapper)
+    #df['Emergencias_cod_corto'] = df['Emergencias_cod_corto'].map(mapper)
     df2 = df.pivot_table(index = 'Periodo', 
             columns = 'Emergencias_cod_corto', 
             fill_value = 0,
             aggfunc = 'count')
-    df2 = df2['Tipo de emergencias']
+    df2 = df2['Tipo_de_emergencias']
     indices = df2.index
     indices.name = None
     columnas = df2.columns
@@ -60,3 +42,8 @@ def line_chart_by_emergencies(df):
     df3 = pd.DataFrame(index = indices, columns = columnas, data = valores)
     df3.index = df3.index.astype(str, copy = False)
     return df3
+
+def line_chart_for_time(df, var):
+    df_time = df.pivot_table(index = ['Periodo'] ,values=var, fill_value = 0, aggfunc = 'median')
+    df_time = df_time[var]
+    return df_time
